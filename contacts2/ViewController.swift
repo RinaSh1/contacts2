f3af0352804ddfb334da50775810379b276cb0cc
RinaSh1 <92697073+RinaSh1@users.noreply.github.com>
2022-05-28 23:53:44 +0200

    Initial Commit

---
+++ contacts2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contacts2
+//
+//  Created by Rina Shabani on 28.5.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
